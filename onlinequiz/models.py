883ccb3af06bbfd5713961dc598fdb3413680619
Gerardo <gerardo@jackslabs.com>
2020-05-05 19:49:50 +0800

    Question models added to models.py

--- onlinequiz/models.py
+++ onlinequiz/models.py
@@ -10,53 +10,147 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(35), unique=True, nullable=False)
   password = db.Column(db.String(25), nullable=False)
   date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  # multichoice_questions_created =  db.relationship('MultichoiceQuestion', lazy=True)
-  # multichoice_questions_assigned = db.relationship('UserMultichoiceQuestion', lazy=True)
+  question_sets =  db.relationship('QuestionSet', backref='owner', lazy=True)
+  multichoice_questions_assigned = db.relationship('UserMultichoiceQuestion', backref='user_assigned', lazy=True)
+  manual_questions_assigned = db.relationship('UserManualQuestion', backref='user_assigned', lazy=True)
+  manual_questions_assigned = db.relationship('UserVotingQuestion', backref='user_assigned', lazy=True)
 
   def __repr__(self):
     return f"User('{self.email}')'"
 
-# class MultichoiceQuestion(db.Model):
-#   __tablename__ = 'MultichoiceQuestion'
-#   id = db.Column(db.Integer, primary_key=True)
-#   question = db.Column(db.String(100), nullable=False)
-#   owner =  db.Column(db.Integer, db.ForeignKey('User.id'))
-#   is_public =  db.Column(db.Boolean(), nullable=True, default=False)
-#   date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#   options =  db.relationship(db.Integer, backref='options', lazy=True)
+class QuestionSet(db.Model):
+  __tablename__ = 'QuestionSet'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(35), nullable=False)
+  owner =  db.Column(db.Integer, db.ForeignKey('User.id'))
+  submitted =  db.Column(db.Boolean(), nullable=True, default=False)
+  is_public =  db.Column(db.Boolean(), nullable=True, default=False)
+  due_date = db.Column(db.DateTime, nullable=True)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  multichoice_question =  db.relationship('MultichoiceQuestion', backref='question_set', lazy=True)
+  manual_question =  db.relationship('ManualQuestion', backref='question_set', lazy=True)
+  voting_question =  db.relationship('VotingQuestion', backref='question_set', lazy=True)
 
-#   def __repr__(self):
-#     return f"MultichoiceQuestion('{self.id}')'"
+  def __repr__(self):
+    return f"QuestionSet('{self.id}')'"
 
-# class MultichoiceOption(db.Model):
-#   __tablename__ = 'MultichoiceOption'
-#   id = db.Column(db.Integer, primary_key=True)
-#   option = db.Column(db.String(100), nullable=False)
-#   is_correct =  db.Column(db.Boolean(), nullable=True, default=False)
-#   date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#   owner = db.Column(db.Integer, db.ForeignKey('MultichoiceQuestion.id'))
+class MultichoiceQuestion(db.Model):
+  __tablename__ = 'MultichoiceQuestion'
+  id = db.Column(db.Integer, primary_key=True)
+  question_set =  db.Column(db.Integer, db.ForeignKey('QuestionSet.id'))
+  question = db.Column(db.String(100), nullable=False)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  options =  db.relationship('MultichoiceOption', backref='multichoice_question', lazy=True)
 
-#   def __repr__(self):
-#     return f"MultichoiceOption('{self.id}')'"
+  def __repr__(self):
+    return f"MultichoiceQuestion('{self.id}')'"
 
-# class UserMultichoiceQuestion(db.Model):
-#   __tablename__ = 'UserMultichoiceQuestion'
-#   __table_args__ = (
-#     db.UniqueConstraint(
-#       'user_id',
-#       'multichoice_question_id',
-#       'multichoice_option_id',
-#       name='unique_user_multichoice_question'
-#     ),
-#   )
-#   id = db.Column(db.Integer, primary_key=True)
-#   due_date = db.Column(db.DateTime, nullable=True)
-#   date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#   user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
-#   multichoice_question_id = db.Column(db.Integer, db.ForeignKey('MultichoiceQuestion.id'))
-#   multichoice_option_id = db.Column(db.Integer, db.ForeignKey('MultichoiceOption.id'))
-#   multichoice_question = db.relationship("MultichoiceQuestion")
-#   multichoice_answer = db.relationship("MultichoiceOption")
+class MultichoiceOption(db.Model):
+  __tablename__ = 'MultichoiceOption'
+  id = db.Column(db.Integer, primary_key=True)
+  option = db.Column(db.String(100), nullable=False)
+  is_correct =  db.Column(db.Boolean(), nullable=True, default=False)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  multichoice_question = db.Column(db.Integer, db.ForeignKey('MultichoiceQuestion.id'))
 
-#   def __repr__(self):
-#     return f"UserMultichoiceQuestion('{self.id}')'"
+  def __repr__(self):
+    return f"MultichoiceOption('{self.id}')'"
+
+class UserMultichoiceQuestion(db.Model):
+  __tablename__ = 'UserMultichoiceQuestion'
+  __table_args__ = (
+    db.UniqueConstraint(
+      'user',
+      'multichoice_question_id',
+      'multichoice_option_id',
+      name='unique_user_multichoice_question'
+    ),
+  )
+  id = db.Column(db.Integer, primary_key=True)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  user = db.Column(db.Integer, db.ForeignKey('User.id'))
+  multichoice_option_id = db.Column(db.Integer, db.ForeignKey('MultichoiceOption.id'))
+  multichoice_question_id = db.Column(db.Integer, db.ForeignKey('MultichoiceQuestion.id'))
+  multichoice_option = db.relationship("MultichoiceOption")
+  multichoice_question = db.relationship("MultichoiceQuestion")
+
+  def __repr__(self):
+    return f"UserMultichoiceQuestion('{self.id}')'"
+
+'''
+  Manual assessment questions
+'''
+class ManualQuestion(db.Model):
+  __tablename__ = 'ManualQuestion'
+  id = db.Column(db.Integer, primary_key=True)
+  question_set =  db.Column(db.Integer, db.ForeignKey('QuestionSet.id'))
+  question = db.Column(db.String(100), nullable=False)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+  def __repr__(self):
+    return f"ManualQuestion('{self.id}')'"
+
+class UserManualQuestion(db.Model):
+  __tablename__ = 'UserManualQuestion'
+  __table_args__ = (
+    db.UniqueConstraint(
+      'user',
+      'manual_question_id',
+      name='unique_user_manual_question'
+    ),
+  )
+  id = db.Column(db.Integer, primary_key=True)
+  answer = db.Column(db.String(100), nullable=True)
+  mark = db.Column(db.String(100), nullable=True)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  user = db.Column(db.Integer, db.ForeignKey('User.id'))
+  manual_question_id = db.Column(db.Integer, db.ForeignKey('ManualQuestion.id'))
+  manual_question = db.relationship("ManualQuestion")
+
+  def __repr__(self):
+    return f"UserManualQuestion('{self.id}')'"
+
+
+'''
+  Voting questions
+'''
+class VotingQuestion(db.Model):
+  __tablename__ = 'VotingQuestion'
+  id = db.Column(db.Integer, primary_key=True)
+  question_set =  db.Column(db.Integer, db.ForeignKey('QuestionSet.id'))
+  question = db.Column(db.String(100), nullable=False)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+  def __repr__(self):
+    return f"VotingQuestion('{self.id}')'"
+
+class VotingOption(db.Model):
+  __tablename__ = 'VotingOption'
+  id = db.Column(db.Integer, primary_key=True)
+  option = db.Column(db.String(100), nullable=False)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  voting_question = db.Column(db.Integer, db.ForeignKey('VotingQuestion.id'))
+
+  def __repr__(self):
+    return f"VotingOption('{self.id}')'"
+
+class UserVotingQuestion(db.Model):
+  __tablename__ = 'UserVotingQuestion'
+  __table_args__ = (
+    db.UniqueConstraint(
+      'user',
+      'voting_question_id',
+      'voting_option_id',
+      name='unique_user_voting_question'
+    ),
+  )
+  id = db.Column(db.Integer, primary_key=True)
+  date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  user = db.Column(db.Integer, db.ForeignKey('User.id'))
+  voting_option_id = db.Column(db.Integer, db.ForeignKey('VotingOption.id'))
+  voting_question_id = db.Column(db.Integer, db.ForeignKey('VotingQuestion.id'))
+  voting_option = db.relationship("VotingOption")
+  voting_question = db.relationship("VotingQuestion")
+
+  def __repr__(self):
+    return f"UserMultichoiceQuestion('{self.id}')'"
